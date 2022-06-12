d1033207ac83e9526914cb8073c5c512ba7dc9c9
yisus44 <jesusadrian1953.1@gmail.com>
2022-06-12 10:27:18 -0500

    modified automation names

--- src/automation/index.ts
+++ src/automation/index.ts
@@ -1,4 +1,4 @@
 import cron from "node-cron";
-import { cronStartGenerateFakeData } from "./meditions-automation/data-generator";
+import { cronStartGenerateFakeData } from "./meditions-utomation/data-generator";
 cronStartGenerateFakeData();
 cron.schedule("*/1 * * * *", cronStartGenerateFakeData);
